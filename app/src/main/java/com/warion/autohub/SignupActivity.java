4b64af19059a51b701920926c4202b7223896e4c
Anthonius Ongkowijoyo <anthonius@komers.id>
2018-02-07 00:16:06 +0700

    Add temporary profile page

--- app/src/main/java/com/warion/autohub/SignupActivity.java
+++ app/src/main/java/com/warion/autohub/SignupActivity.java
@@ -53,7 +53,7 @@ public class SignupActivity extends AppCompatActivity {
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(SignupActivity.this, LoginActivity.class));
+                finish();
             }
         });
 
