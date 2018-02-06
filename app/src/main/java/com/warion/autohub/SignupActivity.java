33881aa4262519181b80057d8386358966863e00
Anthonius Ongkowijoyo <anthonius@komers.id>
2018-02-07 00:07:26 +0700

    Add reset password page

--- app/src/main/java/com/warion/autohub/SignupActivity.java
+++ app/src/main/java/com/warion/autohub/SignupActivity.java
@@ -46,7 +46,7 @@ public class SignupActivity extends AppCompatActivity {
         btnResetPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(SignupActivity.this, LoginActivity.class));
+                startActivity(new Intent(SignupActivity.this, ResetPasswordActivity.class));
             }
         });
 
